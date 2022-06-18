6d005f12766e29cd4153359c6fd55050e36d4c5e
art1work <avermokhin@gmail.com>
2022-06-18 19:37:54 +0300

    Added traffic light switching

--- HW2/ViewController.swift
+++ HW2/ViewController.swift
@@ -18,10 +18,44 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         redLabel.layer.cornerRadius = 50
+        redLabel.layer.opacity = 0.5
         yellowLabel.layer.cornerRadius = 50
+        yellowLabel.layer.opacity = 0.5
         greenLabel.layer.cornerRadius = 50
+        greenLabel.layer.opacity = 0.5
+        
         switchButton.layer.cornerRadius = 8
-    }
+        
 
+    }
+    @IBAction func switchButtonTapped() {
+        switchButton.setTitle("NEXT", for: .normal)
+        changeColor(by: counter)
+        if counter >= 2 {
+            counter = 0
+        } else {
+            counter += 1
+        }
+    }
+    
+    var counter = 0
+    
+    private func changeColor(by counter: Int) {
+        switch counter {
+        case 1:
+            redLabel.layer.opacity = 0.5
+            yellowLabel.layer.opacity = 1
+            greenLabel.layer.opacity = 0.5
+        case 2:
+            redLabel.layer.opacity = 0.5
+            yellowLabel.layer.opacity = 0.5
+            greenLabel.layer.opacity = 1
+        default:
+            redLabel.layer.opacity = 1
+            yellowLabel.layer.opacity = 0.5
+            greenLabel.layer.opacity = 0.5
+        }
+    }
+    
 }
 
