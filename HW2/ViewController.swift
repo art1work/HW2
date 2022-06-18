5ddcd06ee7ed6e56bc154e70f7e6d480b99efdf0
art1work <avermokhin@gmail.com>
2022-06-18 16:08:15 +0300

    Added stack view with traffic light

--- HW2/ViewController.swift
+++ HW2/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLabel: UIView!
+    @IBOutlet weak var yellowLabel: UIView!
+    @IBOutlet weak var greenLabel: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        redLabel.layer.cornerRadius = 75
+        yellowLabel.layer.cornerRadius = 75
+        greenLabel.layer.cornerRadius = 75
     }
 
 
